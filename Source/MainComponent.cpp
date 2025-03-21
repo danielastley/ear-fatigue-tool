7a899f1ddf02875dbda3894f427bd5bc141325ab
Daniel Mark Astley <danielastley@icloud.com>
2025-03-21 12:08:37 +0000

    Changed Initial Text Message

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -18,7 +18,7 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::FontOptions (16.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Ear Fatigue Tool - Work in progress!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
